e5a245c305ac726f2280679b2e081dd0ffa74f87
coffeedood <coffeedood@yahoo.com>
2022-03-26 18:10:27 -0600

    Update square2.py

--- square2.py
+++ square2.py
@@ -1,26 +1,21 @@
 import pygame
 import sys
 from pygame.locals import *
+import random
 
 pygame.init()
 clock = pygame.time.Clock()
 
-surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 GRAY = (150, 150, 150)
 mainLoop = True
 
 # Initialing Color
 color = (255,0,0)
   
-# Drawing Rectangle
-pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
-pygame.display.flip()
-
-xlocation = 0
-ylocation = 0
-
-botxlocation = 600
-botylocation = 600
+rect1 = pygame.Rect(*window.get_rect().center, 0, 0).inflate(75, 75)
+rect2 = pygame.Rect(0, 0, 75, 75)
+rect3 = pygame.Rect(*window.get_rect().bottomleft, 0, 0).inflate(75, 75)
 
 while mainLoop:
     for event in pygame.event.get():
@@ -31,18 +26,17 @@ while mainLoop:
                 mainLoop = False
     keys = pygame.key.get_pressed()
 
-    xlocation += 0.5 * (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT])      
-    ylocation += 0.5 * (keys[pygame.K_DOWN] - keys[pygame.K_UP]) 
+    rect2.move_ip (keys[pygame.K_d] - keys[pygame.K_a], keys[pygame.K_s] - keys[pygame.K_w])
+    window.fill(GRAY)
+    pygame.draw.rect(window, color, rect1)
+    pygame.draw.rect(window, color, rect2)
+    collide = rect1.colliderect(rect2)
+    color = (255, 0, 0) if collide else (255, 255, 255)
+    collide = rect3.colliderect(rect2)
+    color = (255, 0, 0) if collide else (255, 255, 255)
 
-    botxlocation += 0.5 * (keys[pygame.K_d] - keys[pygame.K_a])      
-    botylocation += 0.5 * (keys[pygame.K_s] - keys[pygame.K_d]) 
-     
-    # Drawing Rectangle
-    surface.fill(GRAY)
-    pygame.draw.rect(surface, color, pygame.Rect(xlocation, ylocation, 60, 60))
-    pygame.draw.rect(surface, color, pygame.Rect(botxlocation, botylocation, 60, 60))
-
-    pygame.draw.rect(surface, color, pygame.Rect(600, 650, 60, 60))
-    pygame.draw.rect(surface, color, pygame.Rect(600, 700, 60, 60))
+    window.fill(0)
+    pygame.draw.rect(window, color, rect1)
+    pygame.draw.rect(window, (0, 255, 0), rect2, 6, 1)
     pygame.display.flip()
     clock.tick(120)
